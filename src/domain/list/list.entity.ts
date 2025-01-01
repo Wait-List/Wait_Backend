590cc015b62f6a91ac02045cd0e1de1adccb327b
soohyeon <meltapplee@gmail.com>
2025-01-01 23:52:04 +0900

    setting

---
+++ src/domain/list/list.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+import { User } from "../user/user.entity";
+
+@Entity("tbl_list")
+export class List {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  content: string;
+
+  @Column({ nullable: false })
+  date: Date;
+
+  @Column({ nullable: false })
+  time: string;
+
+  @ManyToOne(() => User, (user) => user.lists)
+  user: User;
+}
