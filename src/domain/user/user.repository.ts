5806c808b20f1450f4bb87640df5af06d95cce0c
soohyeon <meltapplee@gmail.com>
2025-01-21 23:31:23 +0900

    add :: userRepository

---
+++ src/domain/user/user.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./user.entity";
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async save(user: User): Promise<User> {
+    return await this.userRepository.save(user);
+  }
+}
