adcd3ce8f917ab9296080060d4cfaba513126ace
soohyeon <meltapplee@gmail.com>
2025-01-23 23:51:35 +0900

    add :: dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
-
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: "mysql",
       host: process.env.DB_HOST,
